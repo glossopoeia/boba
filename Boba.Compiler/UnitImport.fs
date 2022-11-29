71e8287388052c19f74ebe200f826efe8febf29a
Robert Kleffner <rob.kleffner@gmail.com>
2022-11-28 19:54:22 -0800

    Fix a few list test bugs

--- Boba.Compiler/UnitImport.fs
+++ Boba.Compiler/UnitImport.fs
@@ -69,7 +69,7 @@ module UnitImport =
                 Unit = stringToSmallName "core"
                 Major = intToLiteral "0"
                 Minor = intToLiteral "0"
-                Patch = intToLiteral "17"
+                Patch = intToLiteral "18"
             } }]
 
     let rec loadDependencies alreadySeen imports loaded =
